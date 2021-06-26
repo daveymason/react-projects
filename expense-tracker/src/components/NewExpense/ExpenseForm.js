cbbea6d843568b2c88d4adb7ad3d8d22d3cf7d43
Davey Mason <daveymason@outlook.com>
2021-06-26 19:15:12 +0800

    Added a filter by Year function

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -22,26 +22,29 @@ const ExpenseForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-  
 
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
-    }
+    };
 
     props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
-  }
+  };
 
   return (
     <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input 
+            type='text' 
+            onChange={titleChangeHandler}
+            value={enteredTitle} 
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -49,6 +52,7 @@ const ExpenseForm = (props) => {
             type='number'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -58,6 +62,7 @@ const ExpenseForm = (props) => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
