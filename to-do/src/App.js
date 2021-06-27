28d5e68e0099ce07adbcf231b15bc872684ff5b6
Davey Mason <daveymason@outlook.com>
2021-06-27 09:08:35 +0800

    Project SetUp

---
+++ to-do/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Things to do...</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do/src/components/Todos.js
@@ -0,0 +1,10 @@
+class Todos extends Component{
+    render(){
+    return (
+      <div>
+       <h1>To Do List</h1>
+      </div>
+    )};
+  }
+  
+  export default Todos;
