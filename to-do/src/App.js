403d7b73299127fca15c3aaef1524dce1cf862f0
Davey Mason <daveymason@outlook.com>
2021-06-27 10:29:07 +0800

    Updating ID

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 
   addToList = (title) => {
     const newTodo = {
-      id: 4,
+      id: Math.random().toString(),
       title,
       finished: false
     }
